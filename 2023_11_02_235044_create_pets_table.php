9b4ac1d9487fd8111e8d53ff91dcfa963fa011c9
taka109 <149390595+taka109@users.noreply.github.com>
2023-11-03 01:07:46 +0900

    Add files via upload

---
+++ 2023_11_02_235044_create_pets_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('pets', function (Blueprint $table) {
+            $table->id();
+            $table->int('users_id');
+            $table->varchar('name');
+            $table->varchar('birth');
+            $table->int('animals_id');
+            $table->int('weight');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('pets');
+    }
+};
